90606946becb4a35151d7e538116a32496f83697
Tristan Kenney <tristan@kenney.co>
2021-08-05 18:25:48 +1000

    Fixed styling issues

--- config/features.php
+++ config/features.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'flagr_url' => env('FEATURE_FLAGR_URL', 'http://localhost:18000')
+    'flagr_url' => env('FEATURE_FLAGR_URL', 'http://localhost:18000'),
+    // timeouts in seconds
+    'connect_timeout' => env('FEATURE_CONNECT_TIMEOUT', 1),
+    'timeout' => env('FEATURE_TIMEOUT', 1)
 ];
