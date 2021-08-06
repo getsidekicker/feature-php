2ceaea3d14b7a1ca974aa38f27738d2b18592c68
Tristan Kenney <tristan@kenney.co>
2021-08-06 21:12:06 +1000

    Added messages to Command

--- src/CreateBooleanFlagCommand.php
+++ src/CreateBooleanFlagCommand.php
@@ -3,6 +3,7 @@
 namespace Sidekicker\FlagrFeature;
 
 use Illuminate\Console\Command;
+use Flagr\Client\ApiException;
 
 //Create Laravel Command to create a new flag
 class CreateBooleanFlagCommand extends Command
@@ -22,17 +23,27 @@ class CreateBooleanFlagCommand extends Command
         $description = $this->option('description');
         $tags = $this->option('tags');
 
+        $this->info('Creating flag');
+
         if (!is_string($name) || !is_string($description) || !is_array($tags)) {
             $this->error('Please provide a valid name, description and tags');
 
             return Command::FAILURE;
         }
 
-        $this->booleanFlag->createBooleanFlag(
-            $name,
-            $description,
-            $tags
-        );
+        try {
+            $flag = $this->booleanFlag->createBooleanFlag(
+                $name,
+                $description,
+                $tags
+            );
+
+            $this->info('flag ' . $flag->getKey() . '-' . $flag->getId() . ' created');
+        } catch (ApiException $e) {
+            $this->error($e->getMessage());
+
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
