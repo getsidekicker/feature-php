7b8da854706a7b3795a6423a579f46e91d1726a1
Tristan Kenney <tristan@kenney.co>
2021-08-06 18:00:13 +1000

    Added Auth to tag

--- config/flagr-feature.php
+++ config/flagr-feature.php
@@ -4,5 +4,12 @@ return [
     'flagr_url' => env('FEATURE_FLAGR_URL', 'http://localhost:18000'),
     // timeouts in seconds
     'connect_timeout' => env('FEATURE_CONNECT_TIMEOUT', 1),
-    'timeout' => env('FEATURE_TIMEOUT', 1)
+    'timeout' => env('FEATURE_TIMEOUT', 1),
+    // auth scheme for Create Flag operations
+    // 'none' or 'basic' are valid
+    'auth' => env('FEATURE_AUTH', "none"),
+    'basic' => [
+        'username' => env('FEATURE_AUTH_BASIC_USERNAME'),
+        'password' => env('FEATURE_AUTH_BASIC_PASSWORD'),
+    ]
 ];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,6 +24,9 @@ class TestCase extends TestbenchTestCase
      */
     protected function getEnvironmentSetUp($app): void
     {
-        config()->set('flagr-features.flagr_url', env('FEATURE_FLAGR_URL'));
+        config()->set('flagr-feature.flagr_url', env('FEATURE_FLAGR_URL'));
+        config()->set('flagr-feature.flagr_url', env('FEATURE_FLAGR_URL'));
+        config()->set('flagr-feature.flagr_url', env('FEATURE_FLAGR_URL'));
+        config()->set('flagr-feature.flagr_url', env('FEATURE_FLAGR_URL'));
     }
 }
