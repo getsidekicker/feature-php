d2ede1e970d0398e070b53ecf9b6c2d4dffd55b8
Tristan Kenney <tristan@kenney.co>
2021-08-16 16:50:53 +1000

    Fixed helper signature (#6)

--- src/helpers.php
+++ src/helpers.php
@@ -5,27 +5,25 @@ use Sidekicker\FlagrFeature\Feature;
 if (!function_exists('feature_eval')) {
     /**
      * @param string $flag
-     * @param array<mixed, mixed> $context
      * @param callable ...$callbacks
      * @return void
      */
-    function feature_eval(string $flag, array $context = [], callable ...$callbacks): void
+    function feature_eval(string $flag, callable ...$callbacks): void
     {
-        app(Feature::class)->evaluate($flag, $context, ...$callbacks);
+        app(Feature::class)->evaluate($flag, ...$callbacks);
     }
 }
 
 if (!function_exists('feature_match')) {
     /**
      * @param string $flag
-     * @param array<mixed> $context
      * @param array<mixed>|null $matchAttachment
      * @param string $matchVariant
      * @return boolean
      */
-    function feature_match(string $flag, array $context = [], ?array &$matchAttachment = null, string $matchVariant = 'on'): bool
+    function feature_match(string $flag, ?array &$matchAttachment = null, string $matchVariant = 'on'): bool
     {
-        return app(Feature::class)->match($flag, $context, $matchAttachment, $matchVariant);
+        return app(Feature::class)->match($flag, $matchAttachment, $matchVariant);
     }
 }
 
@@ -35,7 +33,7 @@ if (!function_exists('feature_add_context')) {
      * @param array<mixed> $context
      * @return void
      */
-    function feature_add_context(array $context = []): void
+    function feature_add_context(array $context): void
     {
         app(Feature::class)->addContext($context);
     }
--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -3,6 +3,8 @@
 namespace Sidekicker\FlagrFeature\Tests;
 
 use Illuminate\Http\Request;
+use ReflectionFunction;
+use ReflectionMethod;
 use Sidekicker\FlagrFeature\Feature;
 use Sidekicker\FlagrFeature\FlagrFeatureServiceProvider;
 
@@ -39,6 +41,27 @@ class ServiceProviderTest extends TestCase
         $this->assertTrue(function_exists('feature_add_context'));
     }
 
+    public function testEvalSignature(): void
+    {
+        $evalFunction = new ReflectionFunction('feature_eval');
+        $evalMethod = new ReflectionMethod(Feature::class, 'evaluate');
+        $this->compareParameters($evalMethod->getParameters(), $evalFunction->getParameters());
+    }
+
+    public function testMatchSignature(): void
+    {
+        $evalFunction = new ReflectionFunction('feature_match');
+        $evalMethod = new ReflectionMethod(Feature::class, 'match');
+        $this->compareParameters($evalMethod->getParameters(), $evalFunction->getParameters());
+    }
+
+    public function testAddContextSignature(): void
+    {
+        $evalFunction = new ReflectionFunction('feature_add_context');
+        $evalMethod = new ReflectionMethod(Feature::class, 'addContext');
+        $this->compareParameters($evalMethod->getParameters(), $evalFunction->getParameters());
+    }
+
     public function testAlias(): void
     {
         $this->assertInstanceOf(Feature::class, app('feature'));
@@ -47,5 +70,24 @@ class ServiceProviderTest extends TestCase
     public function testSingleton(): void
     {
         $this->assertEquals(app(Feature::class), app('feature'));
+    }
+
+    /**
+     * @param \ReflectionParameter[] $a
+     * @param \ReflectionParameter[] $b
+     * @return void
+     */
+    private function compareParameters(array $a, array $b): void
+    {
+        $this->assertTrue(count($a) === count($b), 'Parameters count does not match');
+        foreach ($a as $index => $parameter) {
+            $this->assertEquals($parameter->getName(), $b[$index]->getName(), 'Parameter names do not match');
+            $this->assertEquals($parameter->getType(), $b[$index]->getType(), 'Parameter types do not match');
+            $this->assertEquals($parameter->isOptional(), $b[$index]->isOptional(), 'Parameter optional flags do not match');
+            $this->assertEquals($parameter->getType(), $b[$index]->getType(), 'Parameter types do not match');
+            $this->assertEquals($parameter->isPassedByReference(), $b[$index]->isPassedByReference(), 'Parameter reference flags do not match');
+            $this->assertEquals($parameter->isDefaultValueAvailable(), $b[$index]->isDefaultValueAvailable(), 'Parameter default value flags do not match');
+            $this->assertEquals($parameter->isVariadic(), $b[$index]->isVariadic(), 'Parameter variadic flags do not match');
+        }
     }
 }
